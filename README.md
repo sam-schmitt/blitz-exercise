468a463bc05fde92ffddf772212d0c4bda4b7b45
sam-schmitt <samuelgschmitt@gmail.com>
2023-03-04 14:12:45 -0500

    readme

--- README.md
+++ README.md
@@ -3,18 +3,18 @@
 This README contains:
 
 1. A link to a [demo](https://youtube.com/watch?v=9MaZ0NR4_QM&feature=shares)
-2. My notes on my experience
-3. Instructions on installation
+2. My [notes](/README.md#my-notes) on my experience
+3. Instructions on [installation](/README.md#installation)
 
 ## [Demo](https://youtube.com/watch?v=9MaZ0NR4_QM&feature=shares)
 
-# My Notes
+## My Notes
 
 ## The Tree
 
-#### Creating the Tree
+### Creating the Tree
 
-My tree component has nodes that are connected to each other in a parent-child relationship. The tree is represented as a nested data structure, where each node is an object with an id, name, content, and children. The `root` object represents the top-level node of the tree, and it can have any number of child nodes.
+My [tree](/src/components/Tree.tsx) component has [nodes](/src/components/TreeNode.tsx) that are connected to each other in a parent-child relationship. The tree is represented as a nested data structure, where each node is an object with an id, name, content, and children. The `root` object represents the top-level node of the tree, and it can have any number of child nodes.
 
 The tree supports several operations, as they were specified in the assignment: like adding a new child node to a selected parent node, editing a node's content, and deleting a node. I used the `useState()` hook to manage state and render updates. The code uses recursion to traverse the tree to find the selected node and to update the children's content when a parent node is deleted or when new children are added to a node. This tree data structure is used to represent hierarchical data structures like file systems, organization charts, and family trees, and the **interface builder**.
 
@@ -54,11 +54,11 @@ Finally, backend requests can help optimize the performance of the tree by only 
 
 ## The Interface Builder
 
-#### Creating the Builder
+### Creating the Builder
 
-After creating the tree, I knew I needed to reuse the standard tree component in order to show how dynamic it was. The tree accepts a `<ContentController />` prop which is what controls how this instance of the Tree will behave.
+After creating the tree, I knew I needed to reuse the standard tree component in order to show how dynamic it was. The tree accepts a [`<ContentController />`](/src/components/ContentController.tsx) prop which is what controls how this instance of the Tree will behave.
 
-From reading more about what the assignment was asking, I started to understand the purpose of the B1, B2, R1, R2, and R3 datatypes. It certainly is a unique way of classifying these kinds of components.
+From reading more about what the assignment was asking, I started to understand the purpose of the B1, B2, R1, R2, and R3 data types. It certainly is a unique way of classifying these kinds of components.
 
 I decided that in order to simplify the UX of the site, I would have to relay to the user an easier way of understanding what the types meant.
 
@@ -68,7 +68,7 @@ I separated the types into 3 sections: sections, text, and components. These are
 
 I then positioned functional buttons where I though they were best fitting and began to test the UX.
 
-#### Testing the Builder
+### Testing the Builder
 
 In order to test UX, I needed more sets of eyes. I asked friends to take a look and only told them that this was a UI interface builder. They struggled a bit at first, which was expected, as this is a fast-built prototype but I was able to get more feedback that I could put back in the design.
 
@@ -78,7 +78,7 @@ This feedback brought me to implement the drag and drop methods using dndkit and
 
 !['Options'](/assets/options.png)
 
-There are still a few bugs to work through, but nothing a few more casual days of debugging wouldn't fix. One thing that was great to add is the ability to see a preview of your interface as it is being built.
+There are still a few bugs to work through, but nothing a few more casual days of debugging wouldn't fix. One thing that was great to add is the ability to see a [preview](/src/components/PreviewController.tsx) of your interface as it is being built.
 
 _Login Form Being Built with Interface Builder_
 
@@ -88,7 +88,7 @@ _Preview of Built Login Form_
 
 !['Login Preview'](/assets/login-preview.png)
 
-#### What more would I do?
+### What more would I do for the interface builder?
 
 Coding this up was actually very fun, so I might continue this project afterwards for personal amusement.
 
@@ -96,23 +96,31 @@ Coding this up was actually very fun, so I might continue this project afterward
 
 But some things I would add are flex alignment on all components to have a friendly layout customization interface in the group and page section-types. I would also add robust color customization so that builders could create really vivid themes that fit right with their app.
 
-There are also a ton of edge cases unaccounted for in functionality. For example, if a tree moves all the way down to being the last child, it should then automatically move out of the tree it's in and maybe into the next one.
+I also made it very simple to add new component types to the system (which I probably will wind up doing for fun). It's as simple as creating a new editor component and a new preview component and calling them in the `<ContentController />` and `<PreviewController />` respectively.
+
+In terms of UX there are a ton of edge cases unaccounted for in functionality. For example, if a tree moves all the way down to being the last child, it should then automatically move out of the tree it's in and maybe into the next one.
 
 ## The TypeDB Backend
 
-#### Learning TypeDB
+### Learning TypeDB
 
-I've never used **TypeDB** before but that didn't stop me from reading into it. I was impressed by its marketing use-cases and saw why it would be a good fit for an exercise like this. I started to play with TypeDB studio to get a better grasp on how the data could be related. I wrote out and defined the schema for the Tree and it includes entities for the trees and content, relations between parent nodes and their children, relations between references and trees, and relations between nodes and their content.
+I've never used **TypeDB** before but that didn't stop me from reading into it. I was impressed by its marketing use-cases and saw why it would be a good fit for an exercise like this. I started to play with **TypeDB studio** to get a better grasp on how the data could be related. I wrote out and defined the schema for the Tree and it includes:
 
-I then proceeded to sketch out the schema based on the JSON that my root state outputted from the client. I knew that linking to a DB would increase performance of the tree significantly, as you wouldn't need the entire state of the tree to exist on the client, only the visible nodes. So I designed the schema so that you would only ever need to make requests to smaller pieces of the root tree, and you could request the full tree as you went and expanded.
+- attributes to be owned by entities and relations
+- entities for the trees and content
+- relations between parent nodes and their children
+- relations between references and trees
+- relations between nodes and their content
 
-#### Trying Blitz Orm
+I then proceeded to sketch out the schema based on the JSON that my root state outputted from the client. I knew that linking to a DB would increase performance of the tree significantly, as you wouldn't need the entire state of the tree to exist on the client, only the visible nodes. So I designed the [schema](/src/typedb/tql/schema.tql) so that you would only ever need to make requests to smaller pieces of the root tree, and you could request the full tree as you went and expanded.
+
+### Trying Blitz Orm
 
 I'm not going to lie, trying out **Blitz Orm** was a bit difficult but I could see the potential of the new package. Since it is in an early stage, I still made a good attempt to try and get it functional, however, I did experience some compatibility issues with other packages and decided to focus on other aspects of the assignment to better dedicate my time.
 
-I would be very interested in taking a peak into a backend that is already using **Blitz Orm** to its fullest capabilities, so that I could see how well it can fit into a stack. I did take a crack at the schema for **Blitz Orm** and it is based off of the library's syntax.
+I would be very interested in taking a peak into a backend that is already using **Blitz Orm** to its fullest capabilities, so that I could see how well it can fit into a stack. I did take a crack at the [schema](/src/typedb/schema.ts) for **Blitz Orm** and it is based off of the library's syntax.
 
-#### What more would I do?
+### What more would I do for the backend?
 
 I definitely want to do more research on **TypeDB** and **TQL** to just satisfy my curiosity, it seems like a very neat DB solution. As for **Blitz Orm**, it would make the best sense to learn from the developer how it should be applied best, if I'd be given the opportunity to do so.
 
@@ -128,7 +136,7 @@ Feel free after assessing this submission to check out my [portfolio](https://sa
 
 ## Installation
 
-#### To install this package, please follow these steps:
+### To install this package, please follow these steps:
 
 1. Clone this repository
 2. Open a terminal and navigate to the project directory
@@ -136,7 +144,7 @@ Feel free after assessing this submission to check out my [portfolio](https://sa
 
 ## Usage
 
-#### This package contains the following scripts:
+### This package contains the following scripts:
 
 - `dev`: runs the application in development mode
 - `build`: builds the application for production
@@ -147,7 +155,7 @@ To run a script, open a terminal and navigate to the project directory. Then run
 
 ## Dependencies
 
-#### This package has the following dependencies:
+### This package has the following dependencies:
 
 - @blitznocode/blitz-orm: Blitz ORM
 - @dnd-kit/core: DnD Kit core library
@@ -169,7 +177,7 @@ To run a script, open a terminal and navigate to the project directory. Then run
 
 ## Dev Dependencies
 
-#### This package has the following dev dependencies:
+### This package has the following dev dependencies:
 
 - @types/uuid: TypeScript definitions for UUID
 - autoprefixer: Autoprefixer postprocessor
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
 import Header from "@/core/header";
 
 export default function Home(props: any) {
@@ -17,220 +16,18 @@ export default function Home(props: any) {
 			<main className=''>
 				<Header selected={"/"} />
 				<div className='w-3/6 mx-auto mb-10'>
-					<p className='text-center'>
-						<i>My Experience</i>
+					<p className='text-center font-bold'>
+						<i>
+							Welcome! Please use the top tab to navigate and be sure to have
+							read the README in the repository.
+						</i>
 					</p>
-					<p className='text-2xl font-bold mt-7'>The Tree</p>
-					<p className='text-xl mt-5'>Creating the Tree</p>
-					<p className='text-lg mt-2'>
-						My tree component has nodes that are connected to each other in a
-						parent-child relationship. The tree is represented as a nested data
-						structure, where each node is an object with an id, name, content,
-						and children. The `root` object represents the top-level node of the
-						tree, and it can have any number of child nodes.
-					</p>
-					<p className='text-lg mt-2'>
-						The tree supports several operations, as they were specified in the
-						assignment: like adding a new child node to a selected parent node,
-						editing a node's content, and deleting a node. I used the{" "}
-						<code>useState()</code> hook to manage state and render updates. The
-						code uses recursion to traverse the tree to find the selected node
-						and to update the children's content when a parent node is deleted
-						or when new children are added to a node. This tree data structure
-						is used to represent hierarchical data structures like file systems,
-						organization charts, and family trees, and the{" "}
-						<b>interface builder</b>.
-					</p>
-					<p className='text-lg mt-2'>
-						Creating the tree was a bit complicated at first, as I wanted to use
-						a single state variable to control the state of the entire tree,
-						beginning with a root.
-					</p>
-					<p className='text-lg mt-2'>
-						Once I began the <code>addChild</code> function, then I began to
-						figure out how I was going to relate each node and pursue most of
-						the movement functions.
-					</p>
-					<p className='text-xl mt-5'>Testing the Tree</p>
-					<p className='text-lg mt-2'>
-						As I was coding the tree in React, I decided to test it using both
-						the console and the UI. I began by logging the tree structure to the
-						console and verifying that it matched my expectations.
-					</p>
-					<p className='text-lg mt-2'>
-						From there, I used the UI to visualize the tree and identify any
-						issues that needed fixing. As I interacted with the tree in the UI,
-						I noticed that some of the nodes were not behaving as expected.
-					</p>
-					<p className='text-lg mt-2'>
-						By examining the console logs and carefully stepping through the
-						code, I was able to identify most of the issues and make some
-						necessary fixes.
-					</p>
-					<p className='text-lg mt-2'>
-						I learned a lot about state and how it can be managed through a
-						recursive tree. The <code>`createReference`</code>
-						function was the most interesting problem to solve.
-					</p>
-					<p className='text-xl mt-5'>What more would I do?</p>
-					<p className='text-lg mt-2'>
-						If I were to work on a tree data structure again in the future,
-						there are a few things that I would do differently to improve upon
-						it. I would try to write more comprehensive test cases to cover a
-						wider range of potential scenarios for the data. This would allow me
-						to catch any bugs or errors earlier on to save time.
-					</p>
-					<p className='text-lg mt-2'>
-						Additionally, I would make an effort to better organize my code and
-						make it more modular, so that it is easier to read, understand, and
-						maintain over time.
-					</p>
-					<p className='text-lg mt-2'>
-						Finally, backend requests can help optimize the performance of the
-						tree by only retrieving the necessary data on-demand. This can help
-						reduce the amount of data that needs to be stored and processed on
-						the client-side, resulting in faster load times and a more
-						responsive user interface.
-					</p>
-					<p className='text-2xl font-bold mt-7'>The Interface Builder</p>
-					<p className='text-xl mt-5'>Creating the Builder</p>
-					<p className='text-lg mt-2'>
-						After creating the tree, I knew I needed to reuse the standard tree
-						component in order to show how dynamic it was. The tree accepts a{" "}
-						<code>`ContentController`</code> prop which is what controls how
-						this instance of the <b>Tree</b> will behave.
-					</p>
-					<p className='text-lg mt-2'>
-						From reading more about what the assignment was asking, I started to
-						understand the purpose of the B1, B2, R1, R2, and R3 datatypes. It
-						certainly is a unique way of classifying these kinds of components.
-					</p>
-					<p className='text-lg mt-2'>
-						I decided that in order to simplify the UX of the site, I would have
-						to relay to the user an easier way of understanding what the types
-						meant.
-					</p>
-					<p className='text-lg mt-2'>
-						I separated the types into 3 sections: sections, text, and
-						components. These are common ways of referring to types of building
-						blocks for an application.
-					</p>
-					<p className='text-lg mt-2'>
-						I then positioned functional buttons where I though they were best
-						fitting and began to test the UX.
-					</p>
-					<p className='text-xl mt-5'>Testing the Builder</p>
-					<p className='text-lg mt-2'>
-						In order to test UX, I needed more sets of eyes. I asked friends to
-						take a look and only told them that this was a UI interface builder.
-						They struggled a bit at first, which was expected, as this is a
-						fast-built prototype but I was able to get more feedback that I
-						could put bag in the design.
-					</p>
-					<p className='text-lg mt-2'>
-						There are still a few bugs to work through, but nothing a few more
-						casual days of debugging wouldn't fix.
-					</p>
-					<p className='text-lg mt-2'>
-						The concept of a no-code UI interface builder is right there.
-					</p>
-					<p className='text-xl mt-5'>What more would I do?</p>
-					<p className='text-lg mt-2'>
-						Coding this up was actually very fun, so I might continue this
-						project afterwards for personal amusement.
-					</p>
-					<p className='text-lg mt-2'>
-						<i>Not joking.</i>
-					</p>
-					<p className='text-lg mt-2'>
-						But some things I would add are flex alignment on all components to
-						have a friendly layout customization interface in the group and page
-						section-types. I would also add robust color customization so that
-						builders could create really vivid themes that fit right with their
-						app.
-					</p>
-					<p className='text-lg mt-2'>
-						There are also a ton of edge cases unaccounted for in functionality.
-						For example, if a tree moves all the way down to being the last
-						child, it should then automatically move out of the tree it's in and
-						maybe into the next one.
-					</p>
-					<p className='text-2xl font-bold mt-7'>The TypeDB Backend</p>
-					<p className='text-xl mt-5'>Learning TypeDB</p>
-					<p className='text-lg mt-2'>
-						I've never used TypeDB before but that didn't stop me from reading
-						into it. I was impressed by its marketing use-cases and saw why it
-						would be a good fit for an exercise like this. I started to play
-						with TypeDB studio to get a better grasp on how the data could be
-						related. I wrote out and defined the schema for the Tree and it
-						includes entities for the trees and content, relations between
-						parent nodes and their children, relations between references and
-						trees, and relations between nodes and their content. I then
-						proceeded to sketch out the schema based on the JSON that my root
-						state outputted from the client. I knew that linking to a DB would
-						increase performance of the tree significantly, as you wouldn't need
-						the entire state of the tree to exist on the client, only the
-						visible nodes. So I designed the schema so that you would only ever
-						need to make requests to smaller pieces of the root tree, and you
-						could request the full tree as you went and expanded.
-					</p>
-					<p className='text-lg mt-2'>
-						I then proceeded to sketch out the schema based on the JSON that my
-						root state outputted from the client. I knew that linking to a DB
-						would increase performance of the tree significantly, as you
-						wouldn't need the entire state of the tree to exist on the client,
-						only the visible nodes. So I designed the schema so that you would
-						only ever need to make requests to smaller pieces of the root tree,
-						and you could request the full tree as you went and expanded.
-					</p>
-					<p className='text-xl mt-5'>Trying Blitz Orm</p>
-					<p className='text-lg mt-2'>
-						I'm not going to lie, trying out Blitz Orm was a bit difficult but I
-						could see the potential of the new package. Since it is in an early
-						stage, I still made a good attempt to try and get it functional,
-						however, I did experience some compatibility issues with other
-						packages and decided to focus on other aspects of the assignment to
-						better dedicate my time.
-					</p>
-					<p className='text-lg mt-2'>
-						I would be very interested in taking a peak into a backend that is
-						already using Blitz Orm to its fullest capabilities, so that I could
-						see how well it can fit into a stack. I did take a crack at the
-						schema for Blitz Orm and it is based off of the library's syntax.
-					</p>
-					<p className='text-xl mt-5'>What more would I do?</p>
-					<p className='text-lg mt-2'>
-						I definitely want to do more research on TypeDB and TQL to just
-						satisfy my curiosity, it seems like a very neat DB solution. As for
-						Blitz Orm, it would make the best sense to learn from the developer
-						how it should be applied best, if I'd be given the opportunity to do
-						so.
-					</p>
-					<p className='text-lg mt-2'>
-						As state in the assignment details, it is difficult to link JSON to
-						TypeDB, so I would try different formatting on the client to better
-						fit with TQL.
-					</p>
-					<p className='text-2xl font-bold mt-7'>Final Thoughts</p>
-					<p className='text-lg mt-2'>
-						As stated in my brief application (prior to this exercise), I am
-						very interested in building no-code solutions as I've been a part of
-						an entrepreneurial community that has always waited for the day that
-						we could build out all of our ideas without needing to focus on so
-						much code work.
-					</p>
-					<p className='text-lg mt-2'>
-						This exercise was a lot of fun for me, and I would love to continue
-						to work on this problem alongside a strong team of developers,
-						bringing in a polished UX and backend that could serve a
-						distinguished no-code solution to many developers.
-					</p>
-					<p className='text-lg mt-2'>
-						Feel free after assessing this submission to check out my{" "}
+					<p className='mt-5'>
+						Also feel free to check out my{" "}
 						<a
-							target={"_blank"}
 							style={{ color: "blue" }}
 							href='https://samschmitt.net'
+							target={"_blank"}
 						>
 							portfolio
 						</a>
